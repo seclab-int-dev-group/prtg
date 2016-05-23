4574c5d5657e500e4a88b08814f294195b5d56b1
William Thomas Bland <william-thomas-bland@users.noreply.github.com>
2016-05-24 01:07:03 +0200

    Update ip_ping.sh

--- scripts/ip_ping.sh
+++ scripts/ip_ping.sh
@@ -7,7 +7,7 @@
 ###############################################################################################################################
 
 # Declare variables.
-$user=""          # User scripts will run under.
+user=""          # User scripts will run under.
 db="e3db"         # MySQL database name.
 table="e3tb"      # MySQL table name.
 dbuser=""         # MySQL database username
@@ -15,6 +15,8 @@ dbpass=""         # MySQL database password
 pinglog="/home/$user/e3systems/logs/ping"
 
 ###############################################################################################################################
+
+until [ -z "$ping" ] || [ -z "$pktloss" ]; do 
 
 # Populate log file returned ping output.
 ping -c1 "$1" > $pinglog/"$1"
@@ -24,6 +26,8 @@ ping=$(grep 'time=' $pinglog/"$1" | cut -d'=' -f4 | sed 's/ ms//g')
 
 # Populate packet loss variable with current packet loss returned.
 pktloss=$(grep 'packet loss' $pinglog/"$1" | cut -d' ' -f6 | sed 's/%//g')
+
+done
 
 # Output returned value to mysql database
 mysql $db -u$dbuser -p$dbpass -e "UPDATE $table SET Ping='$ping', Packet_loss='$pktloss' WHERE IP_Address='$1';"
