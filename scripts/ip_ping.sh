0bbf8ddddf4d2a5ac1f604c1bb9dd8a25eb72fac
William Thomas Bland <william-thomas-bland@users.noreply.github.com>
2016-05-23 19:40:19 +0200

    Create ip_ping.sh

---
+++ scripts/ip_ping.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+###############################################################################################################################
+#DESCRIPTION:     A custom ping script for compatibility with PRTG Network Monitor.
+#USAGE:           Edit the variables with desired configuration.
+#CREATED BY:      William Thomas Bland.
+###############################################################################################################################
+
+# Declare variables.
+$user=""          # User scripts will run under.
+db="e3db"         # MySQL database name.
+table="e3tb"      # MySQL table name.
+dbuser=""         # MySQL database username
+dbpass=""         # MySQL database password
+pinglog="/home/$user/e3systems/logs/ping"
+
+###############################################################################################################################
+
+# Populate log file returned ping output.
+ping -c1 "$1" > $pinglog/"$1"
+
+# Populate ping variable with current ping returned.
+ping=$(grep 'time=' $pinglog/"$1" | cut -d'=' -f4 | sed 's/ ms//g')
+
+# Populate packet loss variable with current packet loss returned.
+pktloss=$(grep 'packet loss' $pinglog/"$1" | cut -d' ' -f6 | sed 's/%//g')
+
+# Output returned value to mysql database
+mysql $db -u$dbuser -p$dbpass -e "UPDATE $table SET Ping='$ping', Packet_loss='$pktloss' WHERE IP_Address='$1';"
+
+exit 0
