47cf469f531cbe199fbb5477d3998bb3b8e5edb1
William Thomas Bland <william-thomas-bland@users.noreply.github.com>
2016-06-17 21:51:37 +0200

    Update db_add.sh

--- scripts/db_add.sh
+++ scripts/db_add.sh
@@ -9,8 +9,8 @@
 # Declare variables
 db="e3db"           # MySQL database name.
 table="e3tb"        # MySQL table name.
-dbuser=""           # MySQL database username
-dbpass=""           # MySQL database password
+dbuser="e3admin"    # MySQL database username
+dbpass="E3System5!" # MySQL database password
 
 ###############################################################################################################################
 
@@ -19,13 +19,13 @@ mysql $db -u$dbuser -p$dbpass -e "CREATE TABLE $table (\
 IP_Address VARCHAR(16) PRIMARY KEY, \
 Vessel VARCHAR(100), \
 Ping VARCHAR(50), \
-Packet_loss VARCHAR(50), \
+Packet_Loss VARCHAR(50), \
 Latitude VARCHAR(50), \
 Longitude VARCHAR(50), \
 Rx_SNR VARCHAR(50), \
-Rx_raw_reg VARCHAR(50), \
-Rx_raw_reg_lookup VARCHAR(50), \
-Beam_Int VARCHAR(50), \
-Beam_Str VARCHAR(100));"
+Rx_Raw_Reg VARCHAR(50), \
+Rx_Raw_Reg_Lookup VARCHAR(50), \
+Beam_ID VARCHAR(50), \
+Beam_Name VARCHAR(100));"
 
 exit 0
