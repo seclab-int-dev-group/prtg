5f28fd36ea57c2de8617725611b44734db40f551
William Thomas Bland <william-thomas-bland@users.noreply.github.com>
2016-06-21 17:53:02 +0200

    Update ip_add_test.sh

--- scripts/ip_add_test.sh
+++ scripts/ip_add_test.sh
@@ -3,26 +3,29 @@
 for "i" in {1..4}; do
    case "$i" in
    1) if [ -z "$1" ] || [ -z "$2" ] || [ -z "$3" ] || [ -z "$4" ]; then
-        echo "<< ERROR: One of more paramaters are empty. USAGE: ip_add ipaddress username password vessel >>"
-        exit 1
+         echo "<< ERROR: One of more paramaters are empty. USAGE: ip_add ipaddress username password vessel >>"
+         exit 1
       fi
    ;;
    2) if [ -n "$5" ]; then
-        echo "<< ERROR: Spaces found in vessel paramater. Please use underscores >>"
-        exit 2
+         echo "<< ERROR: Spaces found in vessel paramater. Please use underscores >>"
+         exit 2
       fi
    ;;
    3) ping -c5 -t30 "$1" &> /home/e3admin/e3systems/logs/ping/"$1"
       check=$(grep "unknown host" /home/e3admin/e3systems/logs/ping/"$1")
       if [ -n "$check" ]; then
-        echo "<< ERROR: IP address is not correct >>"
-        exit 3
+         echo "<< ERROR: IP address is not correct >>"
+         exit 3
       fi
       ;;
    4) check=$(grep "5 packets transmitted, " /home/e3admin/e3systems/logs/ping/"$1" | cut -d" " -f3)
       if [ $check="0" ]; then
-        echo "<< ERROR: Ping Timeout. Unable to reach IP address >>"
-        exit 4
+         echo "<< ERROR: Ping Timeout. Unable to reach IP address >>"
+         exit 4
+      else
+         ping=$(grep 'rtt min/avg/max/mdev = ' $pinglog/"$1" | cut -d'/' -f5 | sed 's/ ms//g')
+         pktloss=$(grep 'packet loss' $pinglog/"$1" | cut -d' ' -f6 | sed 's/%//g')
       fi
       ;;
    5) /home/e3admin/e3systems/scripts/ip_tel.sh "$1" "$2" "$3" | tee /home/$user/e3systems/logs/raw/"$1"
@@ -38,7 +41,19 @@ for "i" in {1..4}; do
          exit 5
       fi
       ;;
-   6) mysql e3db -ue3admin -pE3System5! -e "INSERT INTO \
+   6) case "$lat" in
+      N) lat=$(echo "$lat" | sed 's/.$//')
+         ;;
+      *) lat=$(echo "-$lat" | sed 's/.$//')
+         ;;
+      esac
+      case "$long" in
+      E) long=$(echo "$long" | sed 's/.$//')
+         ;;
+      *) long=$(echo "-$long" | sed 's/.$//')
+         ;;
+      esac
+      mysql e3db -ue3admin -pE3System5! -e "INSERT INTO \
          e3tb (IP_Address) \
          VALUES (\'$1\');"
       mysql e3db -ue3admin -pE3System5! -e "INSERT INTO \
