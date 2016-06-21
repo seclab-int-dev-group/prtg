2270a6cf317c3e0055b31a55adbca3281d1c7273
William Thomas Bland <william-thomas-bland@users.noreply.github.com>
2016-06-21 17:34:32 +0200

    Update ip_add_test.sh

--- scripts/ip_add_test.sh
+++ scripts/ip_add_test.sh
@@ -43,7 +43,7 @@ for "i" in {1..4}; do
          VALUES (\'$1\');"
       mysql e3db -ue3admin -pE3System5! -e "INSERT INTO \
          e3tb (Vessel) \
-         VALUES ('$vessel') \
+         VALUES ('$(echo "$4" | tr '[:upper:]' '[:lower:]')') \
          WHERE IP_Address=\"$1\";"
       mysql e3db -ue3admin -pE3System5! -e "UPDATE e3tb SET \
          Ping='$ping', \
