df8f792f5c3c9897b193d5fc56ae2628aa19b3c6
William Thomas Bland <william-thomas-bland@users.noreply.github.com>
2016-06-21 18:36:52 +0200

    Update ip_add_test.sh

--- scripts/ip_add_test.sh
+++ scripts/ip_add_test.sh
@@ -1,15 +1,19 @@
 #!/bin/bash
 
-for "i" in {1..4}; do
+for "i" in {1..6}; do
    case "$i" in
    1) if [ -z "$1" ] || [ -z "$2" ] || [ -z "$3" ] || [ -z "$4" ]; then
          echo "<< ERROR: One of more paramaters are empty. USAGE: ip_add ipaddress username password vessel >>"
          exit 1
+      else
+         i=$(( "$i" + 1 ))
       fi
    ;;
    2) if [ -n "$5" ]; then
          echo "<< ERROR: Spaces found in vessel paramater. Please use underscores >>"
          exit 2
+      else
+         i=$(( "$i" + 1 ))
       fi
    ;;
    3) ping -c5 -t30 "$1" &> /home/e3admin/e3systems/logs/ping/"$1"
@@ -17,6 +21,8 @@ for "i" in {1..4}; do
       if [ -n "$check" ]; then
          echo "<< ERROR: IP address is not correct >>"
          exit 3
+      else
+         i=$(( "$i" + 1 ))
       fi
       ;;
    4) check=$(grep "5 packets transmitted, " /home/e3admin/e3systems/logs/ping/"$1" | cut -d" " -f3)
@@ -28,19 +34,23 @@ for "i" in {1..4}; do
          pingmin=$(grep 'rtt min/avg/max/mdev = ' /home/e3admin/e3systems/logs/ping/"$1" | cut -d'/' -f6 | sed 's/ ms//g')
          pingmax=$(grep 'rtt min/avg/max/mdev = ' /home/e3admin/e3systems/logs/ping/"$1" | cut -d'/' -f7 | sed 's/ ms//g')
          pktloss=$(grep 'packet loss' /home/e3admin/e3systems/logs/ping/"$1" | cut -d' ' -f6 | sed 's/%//g')
+      else
+         i=$(( "$i" + 1 ))
       fi
       ;;
    5) /home/e3admin/e3systems/scripts/ip_tel.sh "$1" "$2" "$3" | tee /home/$user/e3systems/logs/raw/"$1"
-      lat=$( grep "latlong = " $rawlog/$1 | cut -d" " -f3-4 | sed 's/\r//g')
-      long=$( grep "latlong = " $rawlog/$1 | cut -d" " -f5-6 | sed 's/\r//g' )
-      rxsnr=$( grep "Rx SNR: " $rawlog/$1 | cut -d" " -f3 | sed 's/\r//g' )
-      rxrr=$( grep "Rx raw reg: " $rawlog/$1 | cut -d" " -f4 | sed 's/\r//g' )
-      rxrrl=$( grep "Rx raw reg lookup: " $rawlog/$1 | cut -d" " -f5 | sed 's/\r//g' )
-      beamid=$( grep " is currently selected" $rawlog/$1 | cut -d" " -f1 | sed 's/\r//g' )
-      beamname=$( grep "$beamid = " $rawlog/$1 | cut -d" " -f3-20 | sed 's/\r//g' )
+      lat=$( grep "latlong = " /home/$user/e3systems/logs/raw/"$1" | cut -d" " -f3-4 | sed 's/\r//g')
+      long=$( grep "latlong = " /home/$user/e3systems/logs/raw/"$1" | cut -d" " -f5-6 | sed 's/\r//g' )
+      rxsnr=$( grep "Rx SNR: " /home/$user/e3systems/logs/raw/"$1" | cut -d" " -f3 | sed 's/\r//g' )
+      rxrr=$( grep "Rx raw reg: " /home/$user/e3systems/logs/raw/"$1" | cut -d" " -f4 | sed 's/\r//g' )
+      rxrrl=$( grep "Rx raw reg lookup: " /home/$user/e3systems/logs/raw/"$1" | cut -d" " -f5 | sed 's/\r//g' )
+      beamid=$( grep " is currently selected" /home/$user/e3systems/logs/raw/"$1" | cut -d" " -f1 | sed 's/\r//g' )
+      beamname=$( grep "$beamid = " /home/$user/e3systems/logs/raw/"$1" | cut -d" " -f3-20 | sed 's/\r//g' )
       if [ "$lat" -ne "*.*" ] && [ "$long" -ne "*.*" ]; then
          echo "<< ERROR: Unable to complete telnet session >>"
          exit 5
+      else
+         i=$(( "$i" + 1 ))
       fi
       ;;
    6) case "$lat" in
